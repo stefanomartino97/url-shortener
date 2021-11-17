c3ead5ff12033f6965a8864dfe0d236442faa99a
Stefano Martino <stefano97martino@gmail.com>
2021-11-17 12:39:23 +0100

    Query parameters support

--- utils/isValidUrl.js
+++ utils/isValidUrl.js
@@ -1,5 +1,8 @@
-const isValidUrl = urlString => {
-  const urlRegex = /^((http(s?)?):\/\/)?([wW]{3}\.)?[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/g;
+const isValidUrl = (urlString) => {
+  const urlRegex =
+    "(\b(https?|ftp|file)://)?[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]";
+
+  ///^((http(s?)?):\/\/)?([wW]{3}\.)?[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/g;
   const result = urlString.match(urlRegex);
   return result !== null;
 };
